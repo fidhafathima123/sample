eba6af695d36aed61fb1746c2d15c5dc7fab90e0
fidhafathima123 <144669671+fidhafathima123@users.noreply.github.com>
2023-09-11 14:29:16 +0530

    Add files via upload

---
+++ circle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+
+float pi=3.14,a,c,r;
+ printf("enter the radius");
+ scanf("%f",&r);
+c=2*pi*r;
+a=pi*r*r;
+
+ printf("circumference:%f\n",c);
+ printf("area:%f",a);
+ return 0;
+ }
+ 
